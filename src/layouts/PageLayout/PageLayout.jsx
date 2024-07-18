9cb65b18b076bed02fc5eda49c9852b9d85b8b90
CyDrops <jbvhert2002@gmail.com>
2024-07-18 19:39:08 +0800

    remove sidebar

--- src/layouts/PageLayout/PageLayout.jsx
+++ src/layouts/PageLayout/PageLayout.jsx
@@ -1,18 +1,9 @@
 import { Flex, Box } from "@chakra-ui/react";
-import { useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
-import Sidebar from "../../components/Sidebar/Sidebar";
 
 const PageLayout = ({ children }) => {
-    const { pathname } = useLocation();
     return (
         <Flex h={"100vh"}>
-            {/* {pathname !== "/auth" ? (
-                <Box w={"240px"}>
-                    <Sidebar />
-                </Box>
-            ) : null} */}
-
             <Box    
                 flex={1}
                 w={{ base: "calc(100% - 70px)", md: "calc(100% - 240px)" }}
