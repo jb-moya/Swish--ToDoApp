a23efa488156011bab171f8556bf4f8c3b0e4ee6
CyDrops <jbvhert2002@gmail.com>
2024-11-27 09:25:00 +0800

    feat: persist category store

--- src/store/categoryStore.js
+++ src/store/categoryStore.js
@@ -1,13 +1,19 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useCategoryStore = create((set) => ({
-    categoriesTaskCount: {},
-    selectedCategoryId: -1,
+const useCategoryStore = create(
+    persist(
+        (set) => ({
+            categoriesTaskCount: {},
+            selectedCategoryId: -1,
 
-    setSelectedCategoryId: (ID) => set({ selectedCategoryId: ID }),
+            setSelectedCategoryId: (ID) => set({ selectedCategoryId: ID }),
 
-    setCategoriesTaskCount: (categoriesTaskCount) =>
-        set({ categoriesTaskCount }),
-}));
+            setCategoriesTaskCount: (categoriesTaskCount) =>
+                set({ categoriesTaskCount }),
+        }),
+        { name: "category-store" } // Key to persist state
+    )
+);
 
 export default useCategoryStore;
